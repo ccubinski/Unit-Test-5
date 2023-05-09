0dfc221f86af141d36516ebdd700aac52379af98
ccubinski <131023725+ccubinski@users.noreply.github.com>
2023-05-09 17:49:33 +0000

    Test Update

--- junit5-parameter-resolution-example/src/test/java/xyz/howtoprogram/junit5/order/EnvironmentInfo.java
+++
@@ -1,9 +0,0 @@
-package xyz.howtoprogram.junit5.order;
-
-public class EnvironmentInfo {
-
-	public String getOS() {
-		return "Windows 10";
-	}
-
-}
--- junit5-parameter-resolution-example/src/test/java/xyz/howtoprogram/junit5/order/ReportService.java
+++
@@ -1,9 +0,0 @@
-package xyz.howtoprogram.junit5.order;
-
-public class ReportService {
-
-	public void report() {
-		//
-	}
-
-}
